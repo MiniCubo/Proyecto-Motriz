dc1923cc80e2e489b97a67924c80336980439e89
MiniCubo <minicubo121@gmail.com>
2025-10-17 19:23:44 -0600

    Sheesh

--- repo_Motriz/Assets/Scripts/FootstepsScript.cs
+++ repo_Motriz/Assets/Scripts/FootstepsScript.cs
@@ -7,19 +7,28 @@ public class FootstepsScript : MonoBehaviour
     public UnityEvent gameWonEvent;
     private RectTransform rt;
     int cont = 0;
+    private float timer;
 
     private void Start()
     {
         rt = GetComponent<RectTransform>();
         rt.position = footstepsPositions[0].position;
+        timer = 0f;
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
     }
 
     public void FootstepReached()
     {
-        if(cont == footstepsPositions.Length -1) gameWonEvent.Invoke();
-        else
+        if (!gameObject.activeSelf) return;
+        if(cont == footstepsPositions.Length -1 ) gameWonEvent.Invoke();
+        else if(timer  < 0f)
         {
             cont++;
+            timer = 0.1f;
             rt.position = footstepsPositions[cont].position;
         }
     }
